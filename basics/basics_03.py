e402b5d187eb956aabfb9536fb7404bd8d00b9b6
Avatur <avaturrr@gmail.com>
2023-02-27 21:10:58 +0300

    basics_03

---
+++ basics/basics_03.py
@@ -0,0 +1,12 @@
+#задание 3
+#создать три переменные, содержащие возраст, найти сумму, вывести на экран
+#создать переменную, равную среднему арифметическому
+
+age_first = 12
+age_second = 34
+age_third = 44
+sum_ages = age_third + age_second + age_third
+print(sum_ages)
+
+sum_ages_mean = (age_third + age_second + age_third) / 3
+print(sum_ages_mean)
