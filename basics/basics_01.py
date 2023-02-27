771cf7574aad5eb00289f29c02a0c00363544e0a
Avatur <avaturrr@gmail.com>
2023-02-27 20:48:06 +0300

    basics_01

---
+++ basics/basics_01.py
@@ -0,0 +1,14 @@
+# задание 1
+# определить переменные всех 4 базовых типов и вывести их на экран
+
+my_int = 23
+print(my_int)
+
+my_float = 1.5
+print(my_float)
+
+my_str = "a;sgrkja;lkj"
+print(my_str)
+
+my_bool = True
+print(my_bool)
