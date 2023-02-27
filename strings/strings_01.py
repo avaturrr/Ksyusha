d7da97d96418dade71eaa806f59c56a09d7f2faf
Avatur <avaturrr@gmail.com>
2023-02-27 21:46:13 +0300

    strings_01

---
+++ strings/strings_01.py
@@ -0,0 +1,9 @@
+#задание 1
+#вывести фио с возрастом
+
+firstname = "Ksjusha"
+lastname = "Satsevich"
+age = "29"
+print("Привет, меня зовут " + firstname + " " + lastname + ", мне " + age + " лет")
+length_str = "Привет, меня зовут " + firstname + " " + lastname + ", мне " + age + " лет"
+print(len(length_str))
