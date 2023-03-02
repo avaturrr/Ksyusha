4931e1eb9b0bdb644deac2f0c914013e2281e09c
Avatur <avaturrr@gmail.com>
2023-03-02 20:02:46 +0300

    collection_03

---
+++ collections/collections_03.py
@@ -0,0 +1,13 @@
+#
+
+my_list_1 = [1, 2, 3, 4, 5, "lasdkfj"]
+my_list_2 = [15, 25, 35, 45, 55, "5lasdkfj5"]
+print(my_list_2)
+my_list_1.extend(my_list_2)
+print(my_list_1)
+my_list_1.insert(4, "123")
+print(my_list_1)
+print(len(my_list_1))
+
+
+
