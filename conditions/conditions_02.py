bf7ceca835310056de9da11abc23ec298f0f1dae
Avatur <avaturrr@gmail.com>
2023-03-06 20:40:23 +0300

    new conditions_02

--- conditions/conditions_02.py
+++ conditions/conditions_02.py
@@ -2,7 +2,7 @@
 вывести на экран Yes, иначе вывести на экран No"""
 
 line = input("Enter sentence: ")
-line.split(" ")
 if "code" in line:
     print("Yes")
-else: print("No")
+else:
+    print("No")
