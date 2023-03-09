ee69dc7c3e8c0e64061d12c18c8c0dc4ec68bf89
Avatur <avaturrr@gmail.com>
2023-03-09 20:43:20 +0300

    new structured_prog_02

--- structured_prog/structured_prog_02.py
+++ structured_prog/structured_prog_02.py
@@ -2,4 +2,4 @@
 длина строки не меньше 8 или в строке есть “python”."""
 
 my_line = input("Enter line: ")
-print(len(my_line) >= 8 and "python" in my_line)
+print(len(my_line) >= 8 or "python" in my_line)
