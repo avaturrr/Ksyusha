09c15b24c102f6f6bacfcec31a3a66e01884e10f
avaturrr <avaturrr@gmail.com>
2023-03-12 19:41:32 +0300

    loops_01

---
+++ loops/loops_01.py
@@ -0,0 +1,7 @@
+"""Выведите на экран n раз фразу "I am a programmer". Число n вводит пользователь."""
+
+n = int(input("Enter number: "))
+i = 0
+while i < n:
+    print("I am a programmer")
+    i += 1
