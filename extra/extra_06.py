ea86747c271441901ff00b518dc1220ac03cccf3
avaturrr <avaturrr@gmail.com>
2023-03-12 15:49:48 +0300

    extra_06

--- extra/extra_06.py
+++ extra/extra_06.py
@@ -2,9 +2,24 @@
 Найти медиану"""
 
 arr = [1, 2, 3, 4, 4, 4, 5, 5, 2]
-if len(arr) % 2 == 0:
-    mediana = (arr[int((len(arr) / 2) - 1)] + arr[int((len(arr) / 2 + 1) - 1)]) / 2
+i = 0
+j = 0
+ordered_arr = []
+while len(arr) != 0:
+    while (arr[j] <= arr[i]):
+        i += 1
+        if i == len(arr):
+            ordered_arr.append(arr.pop(j))
+            break
+    if j < len(arr) - 1: #я думала, может j присваивать тут рандомное число. но ничего не получилось
+        j += 1
+    else:
+        j = 0
+    i = 0
+print(ordered_arr)
+if len(ordered_arr) % 2 == 0:
+    mediana = (ordered_arr[int((len(ordered_arr) / 2) - 1)] + ordered_arr[int((len(ordered_arr) / 2 + 1) - 1)]) / 2
     print(mediana)
 else:
-    mediana = arr[int((len(arr) + 1) / 2) - 1]
+    mediana = ordered_arr[int((len(ordered_arr) + 1) / 2) - 1]
     print(mediana)
