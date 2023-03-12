326d63f472ba61e65205889dca3ea027b3f80f19
avaturrr <avaturrr@gmail.com>
2023-03-12 16:23:28 +0300

    extra_07

--- extra/extra_07.py
+++ extra/extra_07.py
@@ -2,3 +2,25 @@
 айти верхнюю и нижнюю квартиль"""
 
 arr = [1, 2, 3, 4, 4, 4, 5, 5, 2]
+i = 0
+j = 0
+ordered_arr = []
+while len(arr) != 0:
+    while (arr[j] <= arr[i]):
+        i += 1
+        if i == len(arr):
+            ordered_arr.append(arr.pop(j))
+            i = 0
+            break
+    j = i
+print(ordered_arr)
+up_q = 0.75 * (len(ordered_arr) + 1)
+down_q = 0.25 * (len(ordered_arr) + 1)
+if up_q % 1 == 0:
+    print(f"Верхний квартиль: {ordered_arr[int(up_q // 1)]}")
+else:
+    print(f"Верхний квартиль: {(ordered_arr[int(up_q // 1 - 1)] + ordered_arr[int(up_q // 1)]) / 2}")
+if down_q % 1 == 0:
+    print(f"Нижний квартиль: {ordered_arr[int(down_q // 1)]}")
+else:
+    print(f"Нижний квартиль: {(ordered_arr[int(down_q // 1 - 1)] + ordered_arr[int(down_q // 1)]) / 2}")
