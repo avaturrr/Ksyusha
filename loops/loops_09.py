97213db16a37ca6b44e89fe8b1cffa26fdcb4564
avaturrr <avaturrr@gmail.com>
2023-03-12 20:55:18 +0300

    loops_09

---
+++ loops/loops_09.py
@@ -0,0 +1,13 @@
+"""Создать квадратную матрицу размерностью n
+и заполнить ее случайными значениями от 1 до 9."""
+
+n = int(input("Enter number: "))
+my_list = []
+my_list_in = []
+from random import randint
+for i in range(1, n + 1):
+    for j in range(1, n + 1):
+        my_list_in.append(1)
+    my_list.append(list(my_list_in))
+    my_list_in = []
+print(my_list)
