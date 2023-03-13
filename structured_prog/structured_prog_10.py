53a2943819c1f5936b6c1f4c3771efc70f4b225d
Avatur <avaturrr@gmail.com>
2023-03-13 21:54:49 +0300

    structured_prog_11 and 10

--- structured_prog/structured_prog_10.py
+++ structured_prog/structured_prog_10.py
@@ -7,11 +7,18 @@
 предоставить 2 решения. Одно с использованием цикла while, другое с использованием цикла for с параметром.
 Оба решения предоставить в одном файле.
 """
-
+#c циклом for
 my_dict = {'test': 'test_value', 'europe': 'eur', 'dollar': 'usd', 'ruble': 'rub'}
-new_dict = {}
+"""new_dict = {}
 for key in my_dict.keys():
     value = my_dict[key]
     new_key = key + str(len(key))
     new_dict[new_key] = value
-print(new_dict)
+print(new_dict)"""
+#с циклом while
+new_dict = {}
+i = 0
+while i < len(my_dict):
+    print(my_dict.keys())
+    i += 1
+
