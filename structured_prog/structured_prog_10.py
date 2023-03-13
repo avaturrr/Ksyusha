986ee118a5cc427ef95f00d73fb28d46d5195f60
Avatur <avaturrr@gmail.com>
2023-03-13 20:38:32 +0300

    structured_prog_10

---
+++ structured_prog/structured_prog_10.py
@@ -0,0 +1,17 @@
+"""Дан словарь: {'test': 'test_value', 'europe': 'eur', 'dollar': 'usd', 'ruble': 'rub'}
+Добавить каждому ключу число равное длине этого ключа (пример {‘key’: ‘value’} -> {‘key3’: ‘value’}).
+Чтобы получить список ключей - использовать метод .keys()
+
+(подсказка: создается новый ключ с цифрой в конце, старый удаляется)
+
+предоставить 2 решения. Одно с использованием цикла while, другое с использованием цикла for с параметром.
+Оба решения предоставить в одном файле.
+"""
+
+my_dict = {'test': 'test_value', 'europe': 'eur', 'dollar': 'usd', 'ruble': 'rub'}
+new_dict = {}
+for key in my_dict.keys():
+    value = my_dict[key]
+    new_key = key + str(len(key))
+    new_dict[new_key] = value
+print(new_dict)
