6e98defffff945d0588fea1a75a144b4cf986637
avaturrr <avaturrr@gmail.com>
2023-03-21 21:51:17 +0300

    fincs_08

---
+++ funcs/funcs_08.py
@@ -0,0 +1,26 @@
+"""Написать функцию принимающая на вход неопределенным количеством аргументов
+и именованный аргумент mean_type. В зависимости от mean_type вернуть среднеарифметическое
+либо среднегеометрическое. Написать программу в виде трех функций."""
+
+def my_funcs_srar(*args, mean_type = 0):
+    sum = 0
+    for i in args:
+        sum += i
+    sum = sum / len(args)
+    return sum
+def my_funcs_srg(*args, mean_type = 1):
+    sum = 1
+    for i in args:
+        sum *= i
+    sum = sum ** (1 / len(args))
+    return sum
+def my_function(*args, mean_type = 0):
+    if mean_type == 0:
+        return my_funcs_srar(*args)
+    elif mean_type == 1:
+        return my_funcs_srg(*args)
+def main():
+    sum = my_function(4, 4, 2, 2, mean_type = 0)
+    print(f"sum = {sum}")
+if __name__ == '__main__':
+   main()
