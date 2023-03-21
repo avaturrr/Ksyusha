7371cc3f4b6b066b50d6e5745a94c58a1c6f66b3
avaturrr <avaturrr@gmail.com>
2023-03-21 20:28:22 +0300

    funcs_01 and fincs_02

--- funcs/funcs_02.py
+++ funcs/funcs_02.py
@@ -7,7 +7,7 @@
 """
 
 from random import randint
-def my_func_create(n, m):
+def my_func_create(n: int, m: int):
     arr = [[randint(1, 10) for _ in range(n)] for _ in range(m)]
     return arr
 def my_func_output(arr: list):
@@ -21,9 +21,25 @@ def my_func_sum(arr: list):
         sum += sum_01
     return sum
 def my_func_max(arr: list):
-
+    arr_01_max = []
+    for arr_01 in arr:
+        arr_max = max(arr_01)
+        arr_01_max.append(arr_max)
+    my_max = max(arr_01_max)
+    return my_max
+def my_func_min(arr: list):
+    arr_01_min = []
+    for arr_01 in arr:
+        arr_01_min.append(min(arr_01))
+    my_min = min(arr_01_min)
+    return my_min
 
 def main():
-
+    matrix = my_func_create(3, 4)
+    my_func_output(matrix)
+    sum = my_func_sum(matrix)
+    my_max = my_func_max(matrix)
+    my_min = my_func_min(matrix)
+    print(f"sum = {sum}, max = {my_max}, min = {my_min}")
 if __name__ == '__main__':
    main()
