add94cb32f2670ec3a271e7e0d44faf4ac7d9e2a
avaturrr <avaturrr@gmail.com>
2023-03-30 12:21:56 +0300

    func_prog_09

---
+++ func_prog/func_prog_09.py
@@ -0,0 +1,6 @@
+"""Дан список строк. Отформатировать все строки в формате ‘{i} - {string}’,
+где i это порядковый номер строки в списке. Использовать генератор списков.
+"""
+arr = ["alkdj", "jlkneli", "sldkjfo"]
+my_dict ={(key + 1) : value for key, value in enumerate(arr)}
+print(my_dict)
