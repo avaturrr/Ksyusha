0a182ddac7d9ed26404bb74eae09292eb996ffd3
avaturrr <avaturrr@gmail.com>
2023-04-06 00:31:57 +0300

    fin_files_09

--- files/csv_utils.py
+++ files/csv_utils.py
@@ -17,10 +17,9 @@ def read(file):
     return content_my_csv
 
 
-def write(file, fields, rows):
+def write(file,  rows):
     with open(file, "w") as my_csv:
         my_csv_write = csv.writer(my_csv)
-        my_csv_write.writerow(fields)
         my_csv_write.writerows(rows)
     return my_csv_write
 
@@ -43,3 +42,32 @@ def delete_row(file, index=-1):
     else:
         my_list.pop(index)
     write(file, my_list[0], my_list[1::])
+
+
+def my_sum(file):
+    matrix = read(file)
+    cost_goods = 0
+    for i in matrix[1::]:
+        cost_goods += float(i[1]) * float(i[2])
+    return cost_goods
+
+
+def max_price(file):
+    matrix = read(file)
+    max_price = max([float(row[1]) for row in matrix[1::]])
+    row_max_price = [row for row in matrix[1::] if float(row[1]) == max_price]
+    return row_max_price
+
+
+def min_price(file):
+    matrix = read(file)
+    min_price = min([float(row[1]) for row in matrix[1::]])
+    row_min_price = [row for row in matrix[1::] if float(row[1]) == min_price]
+    return row_min_price
+
+def delete_amount(file, row, n=1):
+    matrix = read(file)
+    matrix[row][2] = float(matrix[row][2]) - n
+    write(file, matrix)
+
+
