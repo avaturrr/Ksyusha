a30c532cdd0f659376f37e839627b1d32d2e35ab
avaturrr <avaturrr@gmail.com>
2023-04-10 22:00:27 +0300

    oop_06

--- oop/dog.py
+++ oop/dog.py
@@ -14,7 +14,9 @@ oop_05
 Добавить в класс Dog метод change_name. Метод принимает на вход новое имя
 и меняет атрибут имени у объекта. Создать один объект класса.
 Вывести имя. Вызвать метод change_name. Вывести имя.
-
+oop_06
+Добавить в метод инициализации новый приватный атрибут - master.
+Создать метод get_master() который возвращает значение атрибута master.
 """
 
 
@@ -31,25 +33,31 @@ class Dog:
     def change_name(self, new_name):
         self.name = new_name
 
-    def __init__(self, height, weight, name, age):
+    def get_master(self):
+        return self.__master
+
+    def __init__(self, height, weight, name, age, master):
         self.height = height
         self.weight = weight
         self.name = name
         self.age = age
+        self.__master = master
 
 
-dog_1 = Dog(50, 14, "Bobik", 3)
+dog_1 = Dog(50, 14, "Bobik", 3, "b")
 print(f"height {dog_1.height}, weight {dog_1.weight}, name {dog_1.name}, age {dog_1.age}")
 dog_1.run()
 dog_1.jump()
 dog_1.bark()
-dog_2 = Dog(100, 30, "Tuzik", 4)
+dog_2 = Dog(100, 30, "Tuzik", 4, "t")
 print(f"height {dog_2.height}, weight {dog_2.weight}, name {dog_2.name}, age {dog_2.age}")
 dog_2.jump()
 dog_2.run()
 dog_2.bark()
 
-dog_3 = Dog(13, 2, "Woof", 1)
+dog_3 = Dog(13, 2, "Woof", 1, "w")
 print(f"Dog_3 name {dog_3.name}")
 dog_3.change_name("Foow")
 print(f"Dog_3 name {dog_3.name}")
+
+print(f"dog_3 master {dog_3.get_master()}")
