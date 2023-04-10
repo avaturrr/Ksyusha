d7b77836c46dab59e1f1c451d5adc37799a8fce7
avaturrr <avaturrr@gmail.com>
2023-04-10 21:25:05 +0300

    oop_05

--- oop/dog.py
+++ oop/dog.py
@@ -10,6 +10,11 @@ oop_04
 Создать класс Dog. Класс имеет четыре атрибута: height, weight, name, age.
 Класс имеет три метода: jump, run, bark. Каждый метод выводит сообщение на экран.
 Создать объект класса Dog, вызвать все методы объекта и вывести на экран все его атрибуты.
+oop_05
+Добавить в класс Dog метод change_name. Метод принимает на вход новое имя
+и меняет атрибут имени у объекта. Создать один объект класса.
+Вывести имя. Вызвать метод change_name. Вывести имя.
+
 """
 
 
@@ -22,6 +27,9 @@ class Dog:
 
     def bark(self):
         print("Woof-woof")
+
+    def change_name(self, new_name):
+        self.name = new_name
 
     def __init__(self, height, weight, name, age):
         self.height = height
@@ -40,3 +48,8 @@ print(f"height {dog_2.height}, weight {dog_2.weight}, name {dog_2.name}, age {do
 dog_2.jump()
 dog_2.run()
 dog_2.bark()
+
+dog_3 = Dog(13, 2, "Woof", 1)
+print(f"Dog_3 name {dog_3.name}")
+dog_3.change_name("Foow")
+print(f"Dog_3 name {dog_3.name}")
