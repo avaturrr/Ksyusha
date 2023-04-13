f1c35ca09ccd080851b717f43b9f2b64ac86b05f
Avatur <avaturrr@gmail.com>
2023-04-13 20:59:41 +0300

    OOP_12

--- oop/animals.py
+++ oop/animals.py
@@ -2,9 +2,16 @@
 Атрибуты каждого класса: name, age, master.
 Каждый класс содержит конструктор и методы: run, jump, birthday(увеличивает age на 1), sleep.
 Класс Parrot имеет дополнительный метод fly. Cat - meow, Dog - bark.
+oop_12
+Создать родительский класс Pet, содержащий все общие методы классов Dog, Cat, Parrot.
+Унаследовать Dog, Cat, Parrot от класса Pet.
+Удалить в дочерних классах те методы, которые имеются у родительского класса.
+Создать объект каждого класса и вызвать все его методы.
 """
 
-class Dog:
+
+class Pet:
+
     def __init__(self, name, age, master):
         self.name = name
         self.age = age
@@ -21,52 +28,25 @@ class Dog:
 
     def sleep(self):
         print(f"Sleep {self.name}")
+
+
+class Dog(Pet):
 
     def bark(self):
         print(f"Bark {self.name}")
 
-class Cat:
-    def __init__(self, name, age, master):
-        self.name = name
-        self.age = age
-        self.master = master
 
-    def run(self):
-        print(f"Run {self.name}")
-
-    def jump(self):
-        print(f"Jump {self.name}")
-
-    def birthday(self):
-        self.age += 1
-
-    def sleep(self):
-        print(f"Sleep {self.name}")
+class Cat(Pet):
 
     def meow(self):
         print(f"Meow {self.name}")
 
 
-class Parrot:
-    def __init__(self, name, age, master):
-        self.name = name
-        self.age = age
-        self.master = master
-
-    def run(self):
-        print(f"Run {self.name}")
-
-    def jump(self):
-        print(f"Jump {self.name}")
-
-    def birthday(self):
-        self.age += 1
-
-    def sleep(self):
-        print(f"Sleep {self.name}")
+class Parrot(Pet):
 
     def fly(self):
         print(f"Fly {self.name}")
+
 
 dog_01 = Dog("a", 3, "AF")
 dog_01.bark()
