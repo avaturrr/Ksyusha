936c664c6557d163ad49f85fa9dc4547211db7e3
avaturrr <avaturrr@gmail.com>
2023-04-19 22:47:58 +0300

    oop_26

---
+++ oop/animals_hierarchy.py
@@ -0,0 +1,30 @@
+"""Создать файл animals_hierarchy.py. Реализовать следующую структуру:
+"""
+
+
+class Animal:
+    pass
+
+
+class Pet(Animal):
+    pass
+
+
+class WildAnimal(Animal):
+    pass
+
+
+class Cat(Pet):
+    pass
+
+
+class Dog(Pet):
+    pass
+
+
+class Lion(WildAnimal):
+    pass
+
+
+class Wolf(WildAnimal):
+    pass
