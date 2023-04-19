ebc09b6a3844986a6836fd4f18cd0f4f90ce6f71
avaturrr <avaturrr@gmail.com>
2023-04-19 23:00:11 +0300

    oop_27

--- oop/animals_hierarchy.py
+++ oop/animals_hierarchy.py
@@ -1,9 +1,25 @@
 """Создать файл animals_hierarchy.py. Реализовать следующую структуру:
+oop_27
+Реализовать интерфейсы: Feline(), Canine().
+Все кошачьи должны уметь царапаться. Все собачьи должны уметь выть на луну.
 """
+from abc import ABC, abstractmethod
 
 
 class Animal:
     pass
+
+
+class Feline(ABC):
+    @abstractmethod
+    def scratch(self):
+        raise NotImplementedError
+
+
+class Canine(ABC):
+    @abstractmethod
+    def howl(self):
+        raise NotImplementedError
 
 
 class Pet(Animal):
@@ -14,17 +30,30 @@ class WildAnimal(Animal):
     pass
 
 
-class Cat(Pet):
-    pass
+class Cat(Pet, Feline):
+    def scratch(self):
+        print("scratch-scratch")
 
 
-class Dog(Pet):
-    pass
+class Dog(Pet, Canine):
+    def howl(self):
+        print("howl at the moon")
 
 
-class Lion(WildAnimal):
-    pass
+class Lion(WildAnimal, Feline):
+    def scratch(self):
+        print("scratch-scratch")
 
 
-class Wolf(WildAnimal):
-    pass
+class Wolf(WildAnimal, Canine):
+    def howl(self):
+        print("howl at the moon")
+
+
+def main():
+    lion_alex = Lion()
+    lion_alex.scratch()
+
+
+if __name__ == "__main__":
+    main()
