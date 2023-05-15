5dcf860b86443978cb15851fdf1414fea33c137e
Avatur <avaturrr@gmail.com>
2023-05-15 21:24:05 +0300

    sql_06

--- sql/book.sql
+++ sql/book.sql
@@ -12,6 +12,9 @@ create table book (
 
 alter table book add column release_year integer;
 
-insert into book (title, pages, author, price, release_year) VALUES ('qwe', 300, 'Fds', 20.2, 1993)
-('asd', 340, 'Gfg', 34.4, 2002)
-('hjkl', 350, 'Poi', 38.4, 2013) ('asfjh', 380, 'Pkdf', 3.4, 2007) ('kdld', 340, 'Gfg', 34.4, 2002);
+insert into book (title, pages, author, price, release_year) VALUES ('qwe', 300, 'Fds', 20.2, 1993),
+('asd', 340, 'Gfg', 34.4, 2002),
+('hjkl', 350, 'Poi', 38.4, 2013), ('asfjh', 380, 'Pkdf', 3.4, 2007),
+('kdld', 340, 'Gfg', 34.4, 2002);
+
+select release_year, title, price from book;
