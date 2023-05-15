8196a41b9b552d39472fba7267214c856017fb16
Avatur <avaturrr@gmail.com>
2023-05-15 20:56:38 +0300

    sql_02

---
+++ sql/book.sql
@@ -0,0 +1,11 @@
+-- Создать файл book.sql. Создать таблицу Book.
+-- Атрибуты: id(integer primary key autoincrement),
+-- title(varchar), pages(int), author(varchar), price(float)
+
+create table book (
+    id integer primary key autoincrement,
+    title varchar,
+    pages integer,
+    author varchar,
+    price float
+);
