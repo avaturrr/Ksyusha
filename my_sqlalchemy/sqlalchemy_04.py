b09b01adaf265701d6250ff545e60a3efc18533c
Avaturrr <avaturrr@gmail.com>
2023-05-22 20:38:16 +0300

    slkj

--- my_sqlalchemy/sqlalchemy_04.py
+++ my_sqlalchemy/sqlalchemy_04.py
@@ -18,8 +18,10 @@ user_choice = input("Хотите сохранить эту книгу? y / n")
 engine = create_engine("sqlite:///sa_test.db")
 connection = engine.connect()
 trans = connection.begin()
-insert_book = (f"insert into Book (title, pages, author, price, release_year) values ({title_user}, {pages_user}, {author_user}, {price_user}, {release_year_user});")
-query = text(insert_book)
+insert_book = ("insert into Book (title, pages, author, price, release_year) "
+               "values (:title_user, :pages_user, :author_user, :price_user, :release_year_user);")
+query = text(insert_book).bindparams(title_user=title_user, pages_user=pages_user, author_user=author_user,
+                                     price_user=price_user, release_year_user=release_year_user)
 connection.execute(query)
 if user_choice == "y":
     trans.commit()
