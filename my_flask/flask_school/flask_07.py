5c13fa8f07be5a7f7dbfdb86dc28846cbee3cc2a
avaturrr <avaturrr@gmail.com>
2023-06-25 12:12:49 +0300

    f07

--- my_flask/flask_school/flask_07.py
+++ my_flask/flask_school/flask_07.py
@@ -5,8 +5,10 @@
 
 from flask import Flask, render_template, url_for, redirect, request
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import relationship
 
-app = Flask(__name__, template_folder="/home/user/PycharmProjects/Ksyusha/my_flask/flask_school/templates")
+app = Flask(__name__, template_folder="templates")
 
 app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///flaskschool.db"
 db = SQLAlchemy(app)
@@ -17,14 +19,27 @@ class Group(db.Model):
     fullname = db.Column(db.String(100))
 
 
+class Student(db.Model):
+    id = db.Column("id", db.Integer, primary_key=True)
+    firstname = db.Column(db.String(100))
+    group_id = db.Column(db.Integer, ForeignKey(Group.id), nullable=False)
+    group = relationship("Group", foreign_keys="Student.group_id", backref="students")
+
+
 @app.route("/")
 def read():
     groups = Group.query.all()
     return render_template("07.html", groups=groups)
 
 
+@app.route("/read_students")
+def read_students():
+    students = Student.query.all()
+    return render_template("07_st.html", students=students)
+
+
 @app.route("/add_groups", methods=["POST", "GET"])
-def add_group():
+def add_groups():
     if request.method == "GET":
         return render_template("07_add.html")
     else:
@@ -32,6 +47,48 @@ def add_group():
         db.session.add(group)
         db.session.commit()
         return redirect(url_for("read"))
+
+
+@app.route("/add_student", methods=["POST", "GET"])
+def add_student():
+    if request.method == "GET":
+        return render_template("07_add_st.html")
+    else:
+        student = Student(firstname=request.form["firstname"], group_id=request.form["group_id"])
+        db.session.add(student)
+        db.session.commit()
+        return redirect(url_for("read_students"))
+
+
+@app.route("/update_group", methods=["POST", "GET"])
+def update_group():
+    if request.method == "GET":
+        return render_template("07_update.html", id=request.args.get("id"))
+    else:
+        group = db.session.query(Group). \
+            filter_by(id=request.args.get("id")).first()
+        group.fullname = request.form["fullname"]
+        db.session.commit()
+        return redirect(url_for("read"))
+
+
+@app.route("/update_student", methods=["POST", "GET"])
+def update_student():
+    if request.method == "GET":
+        return render_template("07_update_st.html", id=request.args.get("id"))
+    else:
+        student = db.session.query(Student).filter_by(id=request.args.get("id")).first()
+        student.firstname = request.form["firstname"]
+        student.group_id = request.form["group_id"]
+        db.session.commit()
+        return redirect(url_for("read_students"))
+
+
+@app.route("/delete_group", methods=["GET"])
+def delete_group():
+    db.session.query(Group).filter_by(id=request.args.get("id")).delete()
+    db.session.commit()
+    return redirect(url_for("read"))
 
 
 if __name__ == "__main__":
